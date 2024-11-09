0373c2b811b84b33574ad472b447af9c7e2f93e7
Матвей Бочкарёв <mib15092005@gmail.com>
2024-11-09 20:06:02 +0300

    Hero section without adaptive

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,16 +2,19 @@ export const useBurger = () => {
     const burgerMenu = document.querySelector('.header__right');
     const burgerTrigger = document.querySelector('[data-burger="btn"');
     const burgerOverlay = document.querySelector('[data-burger="overlay"]');
+    const body = document.body;
     const closeBurgerMenu = () => {
         burgerMenu.classList.remove('header__right--visible');
         burgerTrigger.classList.remove('burger--active');
         burgerOverlay.classList.remove('overlay--visible');
+        body.classList.remove('body--fixed');
     };
     closeBurgerMenu();
     burgerTrigger.addEventListener('click', () => {
         burgerMenu.classList.toggle('header__right--visible');
         burgerTrigger.classList.toggle('burger--active');
         burgerOverlay.classList.toggle('overlay--visible');
+        body.classList.toggle('body--fixed');
     });
     burgerOverlay.addEventListener('click', () => {
         closeBurgerMenu();
