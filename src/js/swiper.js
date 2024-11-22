505aff6f723eb91aee6f0ce697de35325dc12056
Матвей Бочкарёв <mib15092005@gmail.com>
2024-11-22 23:27:17 +0300

    Started insight section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,11 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-export const swiper = new Swiper('.swiper', {
-    loop: true,
+export function swiper() {
+    new Swiper('.swiper', {
+        loop: true,
 
-    autoplay: {
-        delay: 2500,
-    },
-});
+        autoplay: {
+            delay: 2500,
+        },
+    });
+}
